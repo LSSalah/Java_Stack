c53989fa56953b7d95baa1b80995edaa0532f004
Salah <74099821+LSSalah@users.noreply.github.com>
2020-12-29 22:47:29 +0200

    Add files via upload

---
+++ JavaFun/Object Master2/Wizard.java
@@ -0,0 +1,23 @@
+package com.codingdojo.java.objectmaster2;
+
+public class Wizard extends Human {
+	
+	public Wizard() {
+		this.health = 50;
+		this.intelligence = 8;
+	}
+	public Wizard(String name) {
+		this.health = 50;
+		this.intelligence = 8;
+		this.name = name;
+	}
+
+	public void heal(Human human) {
+		human.setHealth(human.getHealth()+intelligence);
+		System.out.println("(" + human.getName() + " had their health increased by " + this.intelligence + ".)");
+	}
+	public void fireball(Human human) {
+		human.setHealth(human.getHealth()-(intelligence*3));
+		System.out.println("(" + human.getName() + " has taken " + intelligence*3 + " points of damage!)");
+	}
+}
