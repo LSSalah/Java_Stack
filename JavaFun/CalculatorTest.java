23c40508144115d56073b32b3bf6d14a7e7d14bc
Salah <74099821+LSSalah@users.noreply.github.com>
2020-12-30 17:00:40 +0200

    Add files via upload

---
+++ JavaFun/CalculatorTest.java
@@ -0,0 +1,10 @@
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		Calculator testCal= new Calculator(10.5,"+",5.2);
+		testCal.performOperation();
+		testCal.getResults();
+	}
+
+}
